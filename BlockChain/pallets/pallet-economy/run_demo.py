1a94d668b7e6de5df86d617307168882f2f74bf9
Josephis K Wade <josephiswade397@gmail.com>
2025-06-28 16:40:26 -0600

    Create run_demo.py

---
+++ BlockChain/pallets/pallet-economy/run_demo.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Script to run the Dual-Layer Economy System demo.
+"""
+
+import sys
+import os
+
+# Add the src directory to the Python path
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'src'))
+
+# Import the demo function
+from demo import run_demo
+
+if __name__ == "__main__":
+    run_demo()
