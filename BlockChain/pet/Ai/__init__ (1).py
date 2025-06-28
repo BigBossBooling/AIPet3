1d81f3b26256c8bd8b4616026a595bcb2ed2de35
Josephis K Wade <josephiswade397@gmail.com>
2025-06-28 10:18:50 -0600

    Add files via upload

---
+++ BlockChain/pet/Ai/__init__ (1).py
@@ -0,0 +1,7 @@
+# pet/__init__.py
+"""
+CritterCraft Genesis Pet package.
+This package contains the core functionality for the CritterCraft Genesis Pet system.
+"""
+
+from .pet_core import Pet, InteractionRecord
